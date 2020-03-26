6e3af394fd9dcfa5ad2fc552b5a00a7777b79076
Douglas Jordan <dojordan@microsoft.com>
2020-03-25 18:03:02 -0700

    Fix unit tests

--- test/helper.rb
+++ test/helper.rb
@@ -3,6 +3,8 @@ require "test-unit"
 require "fluent/test"
 require "fluent/test/driver/input"
 require "fluent/test/helpers"
+require 'fluent/plugin/in_snmptrapalert'
+require 'snmp'
 
 Test::Unit::TestCase.include(Fluent::Test::Helpers)
 Test::Unit::TestCase.extend(Fluent::Test::Helpers)
