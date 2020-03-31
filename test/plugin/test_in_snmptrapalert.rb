ddd82df32ad82c1f4a3dc74e6c8e7a636f28b012
Page Bowers <pabowers@microsoft.com>
2020-03-31 11:16:58 -0700

    adding tests to make sure snmp trap v2 works with the plugin

--- test/plugin/test_in_snmptrapalert.rb
+++ test/plugin/test_in_snmptrapalert.rb
@@ -28,13 +28,14 @@ class SnmptrapalertInputTest < Test::Unit::TestCase
     end
   end
 
-#   def send_trap_v2()
-#     SNMP::Manager.open(:Host => "127.0.0.1",:Version => :SNMPv2) do |snmp|
-#       snmp.trap_v2(
-#         "", #Empty string for requestID
-#         [SNMP::VarBind.new("1.3.6.1.2.3.4", SNMP::Integer.new(1))])
-#     end
-#   end
+  def send_trap_v2()
+    SNMP::Manager.open(:Host => "127.0.0.1",:Version => :SNMPv2c) do |snmp|
+      snmp.trap_v2(
+          400, # Fake system up time of 4 seconds
+        "1.3.6.1.4.1.10300.1.1.1.12",
+        [SNMP::VarBind.new("1.3.6.1.2.3.4", SNMP::Integer.new(1))])
+    end
+  end
 
   def test_configure
     driver = create_driver('')
@@ -74,34 +75,34 @@ class SnmptrapalertInputTest < Test::Unit::TestCase
     end
   end
 
-#   test 'emitv2' do
-#     driver = create_driver(SNMP_CONFIG)
-#     driver.run(expect_emits: 1, timeout: 5) do
-#       send_trap_v2()
-#     end
+  test 'emitv2' do
+    driver = create_driver(SNMP_CONFIG)
+    driver.run(expect_emits: 1, timeout: 5) do
+      send_trap_v2()
+    end
 
-#     driver.events.each do |tag, timestamp, trap_events|
-#       assert_equal("SNMPTrap.Alert", tag)
-#       assert_true(timestamp.is_a?(Fluent::EventTime))
-#       assert_equal(trap_events, {"SNMPv2-SMI::mgmt.3.4"=>"1", "host"=>"127.0.0.1"})
-#     end
-#   end
+    driver.events.each do |tag, timestamp, trap_events|
+      assert_equal("SNMPTrap.Alert", tag)
+      assert_true(timestamp.is_a?(Fluent::EventTime))
+      assert_equal(trap_events, {"SNMPv2-MIB::snmpTrapOID.0"=>"SNMPv2-SMI::enterprises.10300.1.1.1.12", "SNMPv2-MIB::sysUpTime.0"=>"00:00:04.00","SNMPv2-SMI::mgmt.3.4"=>"1", "host"=>"127.0.0.1"})
+    end
+  end
 
-#   test 'emit_mibv2' do
-#     driver = create_driver(SNMP_CONFIG)
-#     driver.run(expect_emits: 1, timeout: 5) do
-#       send_trap_v2()
-#     end
-#     driver.events.each do |tag, timestamp, trap_events|
-#       assert_equal 'SNMPTrap.Alert', tag
-#       assert_true timestamp.is_a?(Fluent::EventTime)
-#       message = trap_events.to_s
-#       message = message.delete('\\"')
-#       assert_match(/(?:(SNMPv2-(\w+)(::)(\w+)((\.)(\d+)){1,13}(=>))|(host=>))/, message)
+  test 'emit_mibv2' do
+    driver = create_driver(SNMP_CONFIG)
+    driver.run(expect_emits: 1, timeout: 5) do
+      send_trap_v2()
+    end
+    driver.events.each do |tag, timestamp, trap_events|
+      assert_equal 'SNMPTrap.Alert', tag
+      assert_true timestamp.is_a?(Fluent::EventTime)
+      message = trap_events.to_s
+      message = message.delete('\\"')
+      assert_match(/(?:(SNMPv2-(\w+)(::)(\w+)((\.)(\d+)){1,13}(=>))|(host=>))/, message)
 
-#       trap_events.each do |key, value|
-#         assert_match(/(?:(SNMPv2-(\w+)(::)(\w+)((\.)(\d+)){1,13})|(host))/, key.to_s, "Unknown OID format")
-#       end
-#     end
-#   end
+      trap_events.each do |key, value|
+        assert_match(/(?:(SNMPv2-(\w+)(::)(\w+)((\.)(\d+)){1,13})|(host))/, key.to_s, "Unknown OID format")
+      end
+    end
+  end
 end
